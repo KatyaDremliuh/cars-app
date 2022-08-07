cc98bc0777f3d2e2ed361bfbd668633d6bbb946b
Katsiaryna Dremliuh <nerona.91@gmail.com>
2022-08-07 21:31:18 +0300

    Buttons. Form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'cars-app';
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    car: ['', Validators.required],
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  goScroll(target: HTMLElement) {
+    target.scrollIntoView({behavior: "smooth"})
+  }
+
+  onSubmit() {
+    if(this.priceForm.valid) {
+      alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!")
+    }
+  }
 }
